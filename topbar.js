6c6850db2c7f4a2319b67025ee97d1b6972b3038
bushera <59298086+bushera@users.noreply.github.com>
2020-08-06 00:22:48 +0100

    upto

---
+++ topbar.js
@@ -0,0 +1,51 @@
+$(function(){
+		$("main").show();
+		$("#preload-page").delay(4000).fadeOut(1000);
+  
+
+		$("#year-2018").click(function(){
+			$("#slide2").css("display", "none");
+			$("#slide3").css("display", "none");
+			$("#slide1").css("display", "block");
+			$("#year-2018").off(click);
+		});
+		
+		$("#year-2019").click(function(){
+			$("#slide1").animate({display: "none"}, 500);
+			$("#slide3").css("display", "none");
+			$("#slide2").css("display", "block");
+			$("#year-2019").off(click);
+		});
+		
+		$("#year-2020").click(function(){
+			$("#slide1").animate({display: "none"}, 500);
+			$("#slide2").css("display", "none");
+			$("#slide3").css("display", "block");
+			$("#year-2020").off(click);
+		});
+		
+		
+		$("#year-2018a").click(function(){
+			$("#slide2a").css("display", "none");
+			$("#slide3a").css("display", "none");
+			$("#slide1a").css("display", "block");
+			$("#year-2018a").off(click);
+		});
+		
+		$("#year-2019a").click(function(){
+			$("#slide1a").animate({display: "none"}, 500);
+			$("#slide3a").css("display", "none");
+			$("#slide2a").css("display", "block");
+			$("#year-2019a").off(click);
+		});
+		
+		$("#year-2020a").click(function(){
+			$("#slide1a").animate({display: "none"}, 500);
+			$("#slide2a").css("display", "none");
+			$("#slide3a").css("display", "block");
+			$("#year-2020a").off(click);
+		});
+		
+		
+		
+	});
